08566b168b7feb8da9542879d2520425b70eccca
jocorrea42 <jrcorrearodriguez@gmail.com>
2024-05-07 11:02:58 +0200

    update1

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-mkdir -p /home/jocorrea/ssl
-mkdir -p /home/jocorrea/nginx
-openssl req -x509 -nodes -out /home/jocorrea/ssl/inception.crt -keyout /home/jocorrea/ssl/inception.key -subj "/C=ES/ST=CA/L=Barcelona/O=42/OU=42/CN=jocorrea.42.ba/UID=jocorrea"
+openssl req -x509 -nodes -newkey rsa:2048 -keyout /etc/ssl/private/nginx-inception.key -out $CERT -subj "/C=ES/ST=CA/L=Barcelona/O=42/OU=42/CN=jocorrea.42.ba/UID=jocorrea"
 chmod 755 /var/www/html
 chown -R www-data:www-data /var/www/html
+echo "
+server {
+    listen 443 ssl;
+    listen [::]:443 ssl;
+
+    #server_name www.$DOMAIN_NAME $DOMAIN_NAME;
+
+    ssl_certificate $CERt;
+    ssl_certificate_key /etc/ssl/private/nginx-inception.key;" > /etc/nginx/sites-available/default
+
+
+echo '
+    ssl_protocols TLSv1.3;
+
+    index index.php;
+    root /var/www/html;
+
+    location ~ [^/]\.php(/|$) { 
+            try_files $uri =404;
+            fastcgi_pass wordpress:9000;
+            include fastcgi_params;
+            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        }
+} ' >>  /etc/nginx/sites-available/default
+
 nginx -g "daemon off;"
