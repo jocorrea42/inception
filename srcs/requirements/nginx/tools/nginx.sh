84f85a553b39ad41993accdb8d9135509ed1b829
jocorrea42 <jrcorrearodriguez@gmail.com>
2024-05-07 10:51:24 +0200

    update1

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /home/jocorrea/ssl
 mkdir -p /home/jocorrea/nginx
-openssl req -x509 -nodes -out /etc/nginx/ssl/inception.crt -keyout /etc/nginx/ssl/inception.key -subj "/C=ES/ST=CA/L=Barcelona/O=42/OU=42/CN=jocorrea.42.ba/UID=jocorrea"
+openssl req -x509 -nodes -out /home/jocorrea/ssl/inception.crt -keyout /home/jocorrea/ssl/inception.key -subj "/C=ES/ST=CA/L=Barcelona/O=42/OU=42/CN=jocorrea.42.ba/UID=jocorrea"
 chmod 755 /var/www/html
 chown -R www-data:www-data /var/www/html
 nginx -g "daemon off;"
