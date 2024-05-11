43d81b32c3f3d4ee8995f5afd2795213ba6ab0b2
Jose Ramon Correa Rodriguez <jocorrea@car12s2.42barcelona.com>
2024-05-11 16:44:59 +0200

    step2

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-service mysql start 
+service mysql start
 
 echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
@@ -8,4 +8,4 @@ echo "GRANT ALL PRIVILEGES ON *.* TO '$db_user'@'%' WITH GRANT OPTION;" >> db1.s
 echo "FLUSH PRIVILEGES;" >> db1.sql
 mysql < db1.sql
 kill $(cat /var/run/mysqld/mysqld.pid)
-mysqld --bind-adress=0.0.0.0
+mysqld
