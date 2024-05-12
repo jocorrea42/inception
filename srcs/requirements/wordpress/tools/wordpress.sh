18623e25826e589b702a7c16182bd632dc92031e
jocorrea42 <jrcorrearodriguez@gmail.com>
2024-05-12 12:40:10 +0200

    step 4

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-mkdir /var/www/
-mkdir /var/www/html
-cd /var/www/html
+#mkdir /var/www/
+#mkdir /var/www/html
+#cd /var/www/html
 rm -rf *
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=test --dbpass=12345 --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=jocorrea --admin_password=12345 --admin_email=jocorrea@42.fr --allow-root
+#curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+#chmod +x wp-cli.phar
+#./wp-cli.phar core download --allow-root
+#./wp-cli.phar config create --dbname=wordpress --dbuser=test --dbpass=12345 --dbhost=mariadb --allow-root
+#./wp-cli.phar core install --url=localhost --title=inception --admin_user=jocorrea --admin_password=12345 --admin_email=jocorrea@42.fr --allow-root
 
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's/listen = wordpress:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 mkdir /run/php
 
 #wp redis enable --allow-root
