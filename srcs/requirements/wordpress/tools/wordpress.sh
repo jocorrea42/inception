7321297db64f6062ec698ff60597d8ec7d821c5e
jocorrea42 <jrcorrearodriguez@gmail.com>
2024-05-17 11:14:48 +0200

    new wordpress configured

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -9,18 +9,30 @@ else
 ####### MANDATORY PART ##########
 
 	#Download wordpress and all config file
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+	# wget http://wordpress.org/latest.tar.gz
+	# tar xfz latest.tar.gz
+	# mv wordpress/* .
+	# rm -rf latest.tar.gz
+	# rm -rf wordpress
+	# I need to install wp_cli command and put it in the right directory /usr/local/bin
+  	#Inport env variables in the config file
+	# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
+	# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
+	# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
+	# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	# cp wp-config-sample.php wp-config.php
 
-	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+  	chmod +x wp-cli.phar
+  	mv wp-cli.phar /usr/local/bin/wp
+	#CONFIG
+ 	wp core download --path=$WP_PATH --allow-root
+ 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --path=$WP_PATH --skip-check --allow-root
+  	wp core install --path=$WP_PATH --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --skip-email --allow-root
+ 	wp theme install teluro --path=$WP_PATH --activate --allow-root
+  	wp user create leon leon@le.on --role=author --path=$WP_PATH --user_pass=leon --allow-root
+
+	
 ###################################
 
 ####### BONUS PART ################
